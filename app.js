4148b14926c9f04163c615bb25c391515f0249ec
GabinoJoaquinC <gabinojoaquin@outlook.com>
2025-03-08 22:45:21 -0600

    Se agregaron comentarios al código

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigoSecreto = [];
+let listaAmigoSecreto = []; //Se declara arreglo para guardar lista de amigos
 
+//Funcion para agregar nombre de amigo al arreglo
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
-
+    //Verificar que no se introdujo un campo vacio
     if (nombreAmigo == "") {
         alert("Por favor, inserte un nombre");
     } else {
@@ -18,6 +19,7 @@ function limpiarcaja() {
     document.querySelector('#amigo').value = '';
 }
 
+//Funcion para desplegar lista de amigos en pantalla
 function actualizarLista(amigos,identificador) {
     
     let lista = document.getElementById(identificador);
@@ -31,9 +33,11 @@ function actualizarLista(amigos,identificador) {
     }
   }
 
+// Funcion para elegir a un amigo de la lista en forma aleatoria
 function sortearAmigo() {
     let indice = Math.floor(Math.random()*listaAmigoSecreto.length);
     
+    //Verificar que el arreglo no este vacio
     if (listaAmigoSecreto != "") {
         let lista = document.getElementById('resultado');
         let elemento = document.createElement('li');
@@ -41,6 +45,7 @@ function sortearAmigo() {
         lista.innerHTML = '';
         elemento.textContent = "Tu amigo secreto es: " + listaAmigoSecreto[indice];
         lista.appendChild(elemento);
+        //Vaciar la lista de amigos para que solo se despliegue el amigo sorteado
         lista = document.getElementById('listaAmigos');
         lista.innerHTML = '';
     } else {
